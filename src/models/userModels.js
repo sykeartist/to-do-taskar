46d49f01e4fee936c23ca808ae9fb934fdc87c79
sykeartist <sykeartist@gmail.com>
2024-09-11 09:28:12 +0600

    status update

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -3,7 +3,7 @@
 const mongoose= require("mongoose")
 const userSchema= new mongoose.Schema(
     {
-        name: {type: String}, 
+        fullName: {type: String}, 
         email: {type: String, required: true, unique: true},
         password: String,
         phone: Number,
